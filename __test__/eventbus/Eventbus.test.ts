9fd96b67559601a8a7bdeb2eae63e6935bb3b822
Kaan Alkim <g.kaanalkim@gmail.com>
2020-02-04 20:08:14 +0300

    :sparkles: Eventbus remove publisher method

--- __test__/eventbus/Eventbus.test.ts
+++ __test__/eventbus/Eventbus.test.ts
@@ -2,7 +2,7 @@ import Publisher from "../../src/publisher/publisher";
 import Eventbus from "../../src/eventbus/eventbus";
 
 describe("Publisher's Unit Tests", () => {
-    it("should be initialized with constructor", () => {
+    it("should be initialized with constructor and add a publisher", () => {
         interface Filmography {
             name: string,
             surname: string,
@@ -21,4 +21,27 @@ describe("Publisher's Unit Tests", () => {
         let eventbus = new Eventbus().publisher.add(pub);
 
     });
+
+   /* it("should be remove a publisher item from eventbus", () => {
+        const eventbus = new Eventbus();
+
+        interface Filmography {
+            name: string,
+            surname: string,
+            filmography: Array<string>
+        }
+
+        let pub = new Publisher<Filmography>({
+            topic: "topic_1",
+            state: {
+                name: "Patrick",
+                surname: "Swayze",
+                filmography: ["Ghost"]
+            }
+        });
+
+        eventbus.publisher.add(pub);
+        eventbus.
+
+    });*/
 });
--- src/eventbus/eventbus.ts
+++ src/eventbus/eventbus.ts
@@ -1,15 +1,38 @@
+import Publisher from "../publisher/publisher";
+
 export default class Eventbus<P, S> {
     private _publishers: Array<P> = [];
     private _subscribers: Array<S> = [];
     private _mapper: Array<P | Array<S>>;
     public publisher = {
-        add: this._addPublisher.bind(this)
+        add: this._addPublisher.bind(this),
+        remove: this._removePublisher.bind(this)
     };
 
     private _addPublisher(publisher: P): Eventbus<P, any> {
         this._publishers.push(publisher);
 
         return this;
+    }
+
+    private _removePublisher(publisher: P): Eventbus<P, any> {
+        const idx = this._publishers.indexOf(publisher);
+
+        if (idx !== -1) {
+            this._publishers.splice(idx, 1)
+        }
+
+        return this;
+    }
+
+    public getTopics(): Array<P> {
+        let topics: Array<string> = [];
+
+/*
+        this._publishers.forEach(pub => topics.push(pub.topic))
+*/
+
+        return [];
     }
 
     get publishers(): Array<P> {
