5eb9df9f8ae87aef44055d74a0ccd06158bc6b65
Kaan Alkim <g.kaanalkim@gmail.com>
2020-02-04 21:28:46 +0300

    :sparkles: Subsciber and constructor tests

--- src/subscriber/subsciber.ts
+++ src/subscriber/subsciber.ts
@@ -1,8 +1,26 @@
+interface SubscriberOption {
+    id: string;
+    topic: string,
+    callback: Function
+}
+
 export default class Subsciber {
     private _id: string;
-    topic: string;
+    private _topic: string;
+    private _callback: Function;
+
+    constructor(option?: SubscriberOption) {
+        this._id = option?.id;
+        this._topic = option?.topic;
+        this._callback = option?.callback;
+    }
+
 
     get id(): string {
         return this._id;
+    }
+
+    set id(value: string) {
+        this._id = value;
     }
 }
